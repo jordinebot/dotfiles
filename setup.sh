f653ee2386374f4ef0865611248371fcb5dfc750
Jordi Nebot <jordi@lovetoknow.com>
2018-02-22 16:44:26 +0100

    Moving to ZSH: Add ZIM and change setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,22 @@
-#!/usr/bin/env bash
-ln -fs ~/dotfiles/bash_profile ~/.bash_profile
+#!/usr/bin/env zsh
+
+# Install ZIM if it doesn't exist
+if [ ! -d ${ZDOTDIR:-${HOME}}/.zim ]
+then
+    git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+
+    setopt EXTENDED_GLOB
+    for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+      user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+      touch ${user_file}
+      ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
+    done
+
+    source ${ZDOTDIR:-${HOME}}/.zlogin
+else
+    echo 'Skipping ZIM installation...'
+fi
+
+# ln -fs ~/dotfiles/bash_profile ~/.bash_profile
+ln -fs ~/dotfiles/zshrc ~/.zshrc
 ln -fs ~/dotfiles/gitignore ~/.gitignore
---
+++ zshrc
@@ -0,0 +1,22 @@
+#
+# User configuration sourced by interactive shells
+#
+
+# Change default zim location
+export ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
+
+# Start zim
+[[ -s ${ZIM_HOME}/init.zsh ]] && source ${ZIM_HOME}/init.zsh
+# User configuration sourced by interactive shells
+#
+
+# Change default zim location
+export ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
+
+# Start zim
+[[ -s ${ZIM_HOME}/init.zsh ]] && source ${ZIM_HOME}/init.zsh
+
+
+source ${ZDOTDIR:-${HOME}}/dotfiles/exports
+source ${ZDOTDIR:-${HOME}}/dotfiles/aliases
+
